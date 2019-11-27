998b3a0455e4af9bfed954b7e880619ffaa35046
david.hernando <david.hernandojurado@mytheresa.com>
2019-11-27 09:56:49 +0100

    refactored some code to functions

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ type Column struct {
 // ColumnNameMap tablename --> column ordinal = column name
 type ColumnNameMap = map[string]map[int]string
 
+const (
+	InsertAction = "insert"
+	UpdateAction = "update"
+	DeleteAction = "delete"
+	Database     = "repl"
+)
+
 func main() {
 	cfg := replication.BinlogSyncerConfig{
 		ServerID: 1,
@@ -31,23 +38,85 @@ func main() {
 
 	streamer, err := syncer.StartSync(mysql.Position{Name: "", Pos: 0})
 
-	if err != nil {
-		panic(err)
-	}
+	panicOnErr(err)
 
 	ds := fmt.Sprintf("%s:%s@tcp(%s:%d)/information_schema", cfg.User, cfg.Password, cfg.Host, cfg.Port)
 
 	db, err := sql.Open("mysql", ds)
 
+	cols, err := GetColumnNameMap(db)
+
+	panicOnErr(err)
+
+	for {
+		ev, _ := streamer.GetEvent(context.Background())
+
+		rev, ok := ev.Event.(*replication.RowsEvent)
+
+		if ok {
+			evt := ev.Header.EventType
+
+			action, ok := GetActionFromEventType(evt)
+
+			if !ok {
+				continue
+			}
+
+			schema := string(rev.Table.Schema)
+			table := string(rev.Table.Table)
+
+			if schema != Database {
+				continue
+			}
+
+			fmt.Printf("action: %s - table: %s\n", action, table)
+			for _, row := range rev.Rows {
+				for colIndex, value := range row {
+					colName := cols[table][colIndex+1]
+					if _, ok := value.([]byte); ok {
+						fmt.Printf("byte array - %s: %q\n", colName, value)
+					} else {
+						fmt.Printf("%s: %#v\n", colName, value)
+					}
+				}
+			}
+			fmt.Print("\n")
+		}
+	}
+}
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetActionFromEventType(evt replication.EventType) (string, bool) {
+	var action string
+	switch evt {
+	case replication.WRITE_ROWS_EVENTv2:
+		action = InsertAction
+	case replication.UPDATE_ROWS_EVENTv2:
+		action = UpdateAction
+	case replication.DELETE_ROWS_EVENTv2:
+		action = DeleteAction
+	default:
+		return "", false
+	}
+
+	return action, true
+}
+
+func GetColumnNameMap(db *sql.DB) (ColumnNameMap, error) {
 	colSql := `select TABLE_NAME, 
 					  COLUMN_NAME, 
 				      ORDINAL_POSITION 
 			  	 from COLUMNS
-			  	where TABLE_SCHEMA = 'repl'`
-	colResult, err := db.Query(colSql)
+			  	where TABLE_SCHEMA = ?`
+	colResult, err := db.Query(colSql, Database)
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	var cols = ColumnNameMap{}
@@ -57,8 +126,9 @@ func main() {
 		err := colResult.Scan(&col.TableName, &col.Name, &col.Ordinal)
 
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
+
 		if _, ok := cols[col.TableName]; !ok {
 			cols[col.TableName] = map[int]string{}
 		}
@@ -69,44 +139,8 @@ func main() {
 	err = colResult.Close()
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	for {
-		ev, _ := streamer.GetEvent(context.Background())
-
-		evt := ev.Header.EventType
-
-		switch evt {
-		case replication.UPDATE_ROWS_EVENTv2:
-			fallthrough
-		case replication.DELETE_ROWS_EVENTv2:
-			fallthrough
-		case replication.WRITE_ROWS_EVENTv2:
-			rev, _ := ev.Event.(*replication.RowsEvent)
-			schema := string(rev.Table.Schema)
-			table := string(rev.Table.Table)
-
-			if schema != "repl" {
-				continue
-			}
-
-			fmt.Printf("table: %s\n", table)
-			for _, row := range rev.Rows {
-				for colIndex, value := range row {
-					if _, ok := value.([]byte); ok {
-						fmt.Printf("col index: %d | column: %s | value: %q\n", colIndex, cols[table][colIndex + 1], value)
-					} else {
-						fmt.Printf("col index: %d | column: %s | value: %#v\n", colIndex, cols[table][colIndex + 1], value)
-					}
-				}
-			}
-
-			fmt.Print("\n")
-
-		default:
-			continue
-		}
-
-	}
+	return cols, nil
 }
