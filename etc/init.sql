a242f16334c73ca724aa76c2dc4bb88e2a1967f9
david.hernando <david.hernandojurado@mytheresa.com>
2019-12-07 11:38:28 +0100

    added more types of columns

--- etc/init.sql
+++ etc/init.sql
@@ -1,9 +1,15 @@
 create table people (
-	id int unsigned primary key not null,
+	id int unsigned not null primary key,
 	first_name varchar(50) not null,
-	last_name varchar(50) not null,
 	email varchar(50) not null,
-	gender varchar(50) not null,
-	ip_address varchar(20) not null,
+	gender enum ('Male', 'Female') not null,
+	birthday DATE not null,
+	balance decimal(6,2) not null,
+	is_active bit not null default 0,
+	latitude decimal(7,4) not null,
+	longitude decimal(7,4) not null,
+	breakfast_at time not null,
 	created_at datetime not null
 );
+
+
